d851f1033a08c0555c995ce2ebd6085b75882ae7
MOHAMEDABDULKARIM27 <MohamedAbdulkarim27@gmail.com>
2021-05-23 14:44:14 -0400

    Added Navbar.js,navbar.css, InfoCard.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import Routes from './config/routes.js';
 import './App.css';
-
+import NavBar from './components/NavBar.js';
 
 function App() {
   return (
     <>
+    <NavBar/>
       <Routes />
       {/* <Footer /> */}
     </>
