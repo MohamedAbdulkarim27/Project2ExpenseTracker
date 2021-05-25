d3d6b80612a6a38a2bf41755d56132521a54ccb1
Abrahim Abdulkader <abrahim.abdulkader370@gmail.com>
2021-05-25 12:04:28 -0400

    Created toggle EditForm function with delete fetch call

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,6 +1,26 @@
 import EditForm from "./forms/EditForm"
 const InfoCard = ({ budget }) => {
-  
+  const obj = { userId: localStorage.getItem('userId') };
+  const toggleEditForm = () => {
+    document.getElementById(`budgetCard${budget._id}`).style.display = 'none';
+    document.getElementById(`editForm${budget._id}`).style.display = 'inline';
+  };
+
+  function deleteBudget() {
+    fetch(`https://young-shelf-82889.herokuapp.com/budget/${budget._id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setRefresh(data);
+      })
+      .catch((err) => console.log(err));
+  }
   
   return (
     <>
