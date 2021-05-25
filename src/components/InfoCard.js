6e3596773748790d326a3d04d4c652b24759a53b
Abrahim Abdulkader <abrahim.abdulkader370@gmail.com>
2021-05-25 12:02:22 -0400

    Updated InfoCard and created EditForm

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,14 +1,26 @@
+import EditForm from "./forms/EditForm"
 const InfoCard = ({ budget }) => {
-    return (
-      <>
-        <div className='budgetCont'>
-          <div className='budgetCard'>
-            <span>{budget.name}</span>
-            <span>{budget.expense}</span>
-          </div>
-        </div>
-      </>
-    );
-  };
   
-  export default InfoCard;
+  
+  return (
+    <>
+      <div className='budgetCont'>
+        <div id={`budgetCard${budget._id}`} className='budgetCard'>
+          <span>{budget.name}</span>
+          <span>{budget.expense}</span>
+        </div>
+        <EditForm index={index} budget={budget} setRefresh={setRefresh} />
+        <span onClick={() => toggleEditForm()}>
+          <i className='fas fa-marker'></i>
+        </span>
+        <span onClick={() => deleteBudget(budget._id)}>
+          <i className='fas fa-trash-alt'></i>
+        </span>
+      </div>
+      <hr />
+      <br />
+    </>
+  );
+};
+
+export default InfoCard;
